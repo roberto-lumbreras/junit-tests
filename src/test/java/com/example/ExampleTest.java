a2e1ae232d11f6e35f0aef145e509a6aa7ea17de
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-07 18:12:31 +0200

    First commit

---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    private Example example = new Example();
+
+    // 1. Test para sumar dos números
+    @Test
+    public void testSumar() {
+        
+    }
+
+    // 2. Test para checkPositivo con excepción
+    @Test
+    public void testCheckPositivo() {
+        
+    }
+
+    // 3. Test para contar letras 'a'
+    @Test
+    public void testContarLetrasA() {
+        
+    }
+
+    // 4. Test para contieneElemento
+    @Test
+    public void testContieneElemento() {
+        
+    }
+
+    // 5. Test para revertirCadena
+    @Test
+    public void testRevertirCadena() {
+        
+    }
+
+    // 6. Test para factorial
+    @Test
+    public void testFactorial() {
+        
+    }
+
+    // 7. Test para esPrimo
+    @Test
+    public void testEsPrimo() {
+        
+    }
+
+    // 8. Test para mensajeConRetraso
+    @Test
+    public void testMensajeConRetraso() {
+        
+    }
+
+    // 9. Test para convertirAString
+    @Test
+    public void testConvertirAString() {
+        
+    }
+
+    // 10. Test para calcularMedia
+    @Test
+    public void testCalcularMedia() {
+        
+    }
+
+    // 11. Test para convertirListaAString
+    @Test
+    public void testConvertirListaAString() {
+        
+    }
+}
